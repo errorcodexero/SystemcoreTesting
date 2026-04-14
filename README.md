34a12e62cf2d29f62d730361b24d3fb34438ba26
blaze-developer <bryceblazegaming@gmail.com>
2026-04-13 22:44:37 -0700

    readme and flakes

---
+++ README.md
@@ -0,0 +1,3 @@
+# SystemcoreTesting
+
+This repository is the source code for 1425's offseason testing of the new 2027 FIRST control system, Systemcore, and its tooling.
---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      version = "2026.2.1";
+
+      wpilibJdk =
+        pkgs.runCommand "wpilib-jdk"
+          {
+            src = pkgs.fetchzip {
+              url = "https://packages.wpilib.workers.dev/installer/v${version}/Linux/WPILib_Linux-${version}.tar.gz";
+              hash = "sha256-yly96Zzj0jnvudZhwE30GqvK3Ny2Tqot84ZbPS0RrFM=";
+            };
+          }
+          ''
+            mkdir -p $out
+
+            tar -xzf $src/WPILib_Linux-${version}-artifacts.tar.gz jdk
+            cp -r jdk/* $out/
+          '';
+
+      deployScript =
+        let
+          source = builtins.readFile ./deptuning.sh;
+        in
+        pkgs.writeShellScriptBin "deploytuning" source;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        name = "relic";
+
+        packages = [ wpilibJdk pkgs.libGL deployScript ];
+
+        JAVA_HOME = wpilibJdk;
+      };
+    };
+}
